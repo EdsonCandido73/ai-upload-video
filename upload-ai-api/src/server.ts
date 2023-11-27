091aace5e27a91be6e0ea07c3717daa51db0bd3c
Edson Candido <edson.cursoprogramacao@gmail.com>
2023-11-27 18:25:25 -0300

    Initial commit

---
+++ upload-ai-api/src/server.ts
@@ -0,0 +1,25 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
+
+const app = fastify()
+
+
+// colocar exatamente a URL do seu site para bloquear acessos de outras origens
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server running')
+})
